59b426abd6ae307410b9d9b30d10cd206795805a
DiamondDev <fynndiamond@gmail.com>
2024-12-09 18:12:54 +0000

    remove src, moving to another repo

--- nvcm-src/script/game/crosshair.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-class_name Crosshair
-
-
-func _process(_delta):
-	global_position = get_global_mouse_position()
--- nvcm-src/script/hud/money_label.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-class_name MoneyLabel
-
-@onready var label: Label = $HBoxContainer/Label
-
-var amount: int = 0
-
-func _process(_delta):
-	label.text = str(amount)
